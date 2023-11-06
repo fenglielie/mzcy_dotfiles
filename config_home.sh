83ceaefaba40efd9ba3a486c84841996ecb5503a
fenglielie <fenglielie@qq.com>
2023-11-07 02:51:18 +0800

    modify config_home.sh a little

--- config_home.sh
+++ config_home.sh
@@ -4,13 +4,13 @@
 script_root_dir=$(dirname "$(realpath "$0")")
 
 # tmux
-ln -sf $script_root_dir/tmux/.tmux.conf ~/.tmux.conf
+ln -sf $script_root_dir/tmux/.tmux.conf $HOME/.tmux.conf
 echo "tmux config finished."
 
 # vim
-ln -sf $script_root_dir/vim/.vimrc ~/.vimrc
+ln -sf $script_root_dir/vim/.vimrc $HOME/.vimrc
 echo "vim config finished."
 
 # git
-ln -sf $script_root_dir/git/.gitconfig ~/.gitconfig
+ln -sf $script_root_dir/git/.gitconfig $HOME/.gitconfig
 echo "git config finished."
