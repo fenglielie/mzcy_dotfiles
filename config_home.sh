27826896d11e21a9641d00f824423bafc45147c0
fenglielie <fenglielie@qq.com>
2023-11-07 00:40:06 +0800

    fix a bug in bash

--- config_home.sh
+++ config_home.sh
@@ -4,13 +4,13 @@
 script_root_dir=$(dirname "$(realpath "$0")")
 
 # tmux
-ln -sf $(script_root_dir)/tmux/.tmux.conf ~/.tmux.conf
+ln -sf $script_root_dir/tmux/.tmux.conf ~/.tmux.conf
 echo "tmux config finished."
 
 # vim
-ln -sf $(script_root_dir)/vim/.vimrc ~/.vimrc
+ln -sf $script_root_dir/vim/.vimrc ~/.vimrc
 echo "vim config finished."
 
 # git
-ln -sf $(script_root_dir)/git/.gitconfig ~/.gitconfig
+ln -sf $script_root_dir/git/.gitconfig ~/.gitconfig
 echo "git config finished."
